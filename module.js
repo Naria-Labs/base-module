b84e04e56311c9dd02f81ca00466939b4a8de5cb
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-25 23:14:53 +0100

    Update module.js
    revert

--- module.js
+++ module.js
@@ -1,6 +1,5 @@
 module.exports = {
   commands: [
-    //for the future this line makes the pipeline fail prob smth with either command not implemented or smth else
-    //require('./utils/ping.js'),
+    require('./utils/ping.js'),
   ]
 }
